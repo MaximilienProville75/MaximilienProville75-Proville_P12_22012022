624ed92e540795de36f9a8acf041a763aea8798d
PROVILLE Maximilien <maximilien.proville77@gmail.com>
2022-01-22 20:24:32 +0100

    22-01 20:24 --> Updated Root & data logics

--- sportsee/src/components/App/App.js
+++ sportsee/src/components/App/App.js
@@ -2,8 +2,23 @@ import "./App.css";
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import Dashboard from "../DashBoard/DashBoard";
 
+//TODO Make different route depending on the idea type in
+//TODO Then generate the data base on the Id passed in
+//TODO Make also a 404 error page Route
+
+import { USER_MAIN_DATA } from "../../data";
+console.log(USER_MAIN_DATA.id);
+// const {..} = USER_MAIN_DATA;
+// console.log(id);
+
 function App() {
-  return <Dashboard />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
