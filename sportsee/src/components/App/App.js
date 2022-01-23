21a2b669e443396929c9ce1cd493e711a2fcf16e
PROVILLE Maximilien <maximilien.proville77@gmail.com>
2022-01-23 23:31:19 +0100

    23-01 23:31 --> Last Daily Update

--- sportsee/src/components/App/App.js
+++ sportsee/src/components/App/App.js
@@ -6,8 +6,16 @@ import Dashboard from "../DashBoard/DashBoard";
 //TODO Then generate the data base on the Id passed in
 //TODO Make also a 404 error page Route
 
-import { USER_MAIN_DATA } from "../../data";
-console.log(USER_MAIN_DATA.id);
+import {
+  USER_MAIN_DATA,
+  USER_ACTIVITY,
+  USER_AVERAGE_SESSIONS,
+  USER_PERFORMANCE,
+} from "../../data";
+console.log(USER_MAIN_DATA[0]);
+console.log(USER_ACTIVITY[0]);
+console.log(USER_AVERAGE_SESSIONS[0]);
+console.log(USER_PERFORMANCE[0]);
 // const {..} = USER_MAIN_DATA;
 // console.log(id);
 
