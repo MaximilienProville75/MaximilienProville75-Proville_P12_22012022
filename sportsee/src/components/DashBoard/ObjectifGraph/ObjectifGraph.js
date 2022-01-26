7517884d148851d873cfb2e5837a1d3caa834b35
PROVILLE Maximilien <maximilien.proville77@gmail.com>
2022-01-26 14:38:00 +0100

    26-01 14:37 --> DailyGraph Update

--- sportsee/src/components/DashBoard/ObjectifGraph/ObjectifGraph.js
+++ sportsee/src/components/DashBoard/ObjectifGraph/ObjectifGraph.js
@@ -7,50 +7,37 @@ import {
   YAxis,
   Tooltip,
   Legend,
+  Label,
 } from "recharts";
 
 const data = [
   {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
+    name: "L",
+    uv: 30,
   },
   {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
+    name: "M",
+    uv: 23,
   },
   {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
+    name: "M",
+    uv: 45,
   },
   {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
+    name: "J",
+    uv: 50,
   },
   {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
+    name: "V",
+    uv: 0,
   },
   {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
+    name: "S",
+    uv: 0,
   },
   {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
+    name: "S",
+    uv: 60,
   },
 ];
 
@@ -62,13 +49,13 @@ function ObjectifGraph() {
         height={260}
         data={data}
         margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        backgroundColor="#FF0000"
       >
-        <XAxis dataKey="name" />
+        <XAxis dataKey="name"></XAxis>
         <YAxis />
         <Tooltip />
         <Legend />
-        <Line type="monotone" dataKey="pv" stroke="#8884d8" />
-        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        <Line type="monotone" dataKey="uv" stroke="#FF0000" />
       </LineChart>
     </div>
   );
