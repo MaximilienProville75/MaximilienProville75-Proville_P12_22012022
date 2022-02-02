665b3187da86ff05cf9974f3a1669f871ad4031b
PROVILLE Maximilien <maximilien.proville77@gmail.com>
2022-02-02 12:17:34 +0100

    02-02 12:17 --> Env / Models / Services ongoing

--- sportsee/src/model/index.js
+++ sportsee/src/model/index.js
@@ -1,9 +1,12 @@
-// import { getUser } from "../service";
-import { getUser } from "../mockService";
+import * as apiService from "../service";
+import * as mockService from "../mockService";
+
+const service =
+  process.env.REACT_APP_MOCK_ACTIVE === "true" ? mockService : apiService;
 
 export class Model {
   static async getUser(userId) {
-    const result = await getUser(userId);
+    const result = await service.getUser(userId);
     return {
       id: result.id,
       keyData: result.keyData,
